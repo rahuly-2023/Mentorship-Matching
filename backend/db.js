1db3a74efa9d5dbcfe41d6ca3ed72f1c3e0182b8
“Rahul <rahul2483yadav@gmail.com>
2025-01-06 21:22:12 +0530

    Databae configuration

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root', // replace with your MySQL username
-    password: 'rahul', // replace with your MySQL password
-    database: 'mentorship_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, // replace with your MySQL username
+    password: process.env.DB_PASSWORD, // replace with your MySQL password
+    database: process.env.DB_NAME,
 });
 
 console.log("db success");
